0e9aaf64dd29953fb6802209a941464cb4f38e3d
Marx02 <casper_marx@msn.com>
2019-09-04 12:57:31 +0200

    first

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    [SerializeField] private Transform levelPart_Start;
+    [SerializeField] private Transform levelPart_1;
+
+
+    private void Awake()
+    {
+        Instantiate(levelPart_1, new Vector3 (-5, -4), Quaternion.identity);
+    }
+
+
+    private void spawnLevelPart(Vector3 spawnPosistion)
+    {
+        Instantiate(levelPart_1, spawnPosistion, Quaternion.identity);
+    }
+
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject spikes;
+
+    private float timeBtwnSpawn;
+    public float startTimeBtwnSpawn;
+
+    private void Update()
+    {
+        if (timeBtwnSpawn <= 0)
+        {
+            Instantiate(spikes, transform.position, Quaternion.identity);
+            timeBtwnSpawn = startTimeBtwnSpawn;
+        }
+        else{
+            timeBtwnSpawn -= Time.deltaTime;
+        }
+    }
+
+}
